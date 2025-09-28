282976ea8718066222a3ea0a33752832f265f46c
David Johns <djjohns42@gmail.com>
2025-09-28 13:49:01 -0500

    Added docstring to get_history_df.py

--- scripts/utilities/get_history_df.py
+++ scripts/utilities/get_history_df.py
@@ -2,8 +2,15 @@ import pygmc
 import pandas as pd
 
 
-def get_history_df():
+def get_history_df() -> pd.DataFrame:
+    """
+    Fetches device's history as a list of tuples where the first row is column names and converts it to a pandas dataframe. Since the device's onboard ram is only 2MB with data logging size capped around 300KB before I started noticing issues with fetching the data from the device we can get away with just returning the pandas dataframe.
+
+    Returns:
+    df (DataFrame): Columns: "datetime", "count", "unit", "mode", "reference_datetime", "notes"
+    """
     gc = pygmc.GMCSE(port="COM3")
+    # gc = pygmc.connect()
 
     gc.get_version()
     gc.get_serial()
