7d3c4c97cfa639f3d0c2b8256f8297a550783e47
David Johns <djjohns42@gmail.com>
2025-09-27 21:15:18 -0500

    Added a get_history_length module.

---
+++ utilities/get_history_length.py
@@ -0,0 +1,17 @@
+import pygmc
+
+
+def get_history_length():
+    gc = pygmc.GMCSE(port="COM3")
+
+    gc.get_version()
+    gc.get_serial()
+
+    history = gc.get_history_data()
+    row_count = len(history)
+    print(f"Current row count: {row_count}")
+    return row_count
+
+
+if __name__ == "__main__":
+    get_history_length()
