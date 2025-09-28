5ce3825044bdd348dd6a55a2fb62f374b7acb00d
David Johns <djjohns42@gmail.com>
2025-09-28 13:32:55 -0500

    Added docstring for get_device_log.py

--- scripts/utilities/get_device_log.py
+++ scripts/utilities/get_device_log.py
@@ -2,19 +2,26 @@ import pygmc
 from datetime import datetime
 
 
-def get_device_log():
-    gc = pygmc.GMCSE(port="COM3")
+def get_device_log(base_filename="GMC-SE", data_dir="./data/") -> None:
+    """
+    Pulls device's log and writes it to GMC-SE_YYYYMMDD.csv in the repo's data dir.
+
+    #### KWARGS:
+    base_filename (str): Optional, base filename. Default: ``GMC-SE``.
+
+    data_dir (str): Optional, path to where we write the file. Default: ``./data/``
+    """
+    # gc = pygmc.GMCSE(port="COM3")
+    gc = pygmc.connect()
 
     gc.get_version()
     gc.get_serial()
 
     # Pull log from GMC-SE
-    base_filename = "GMC-SE"
-    # ds = datetime.now().strftime("_%Y%m%d_%H%M%S")
-    ds = datetime.now().strftime("_%Y%m%d")
+    # ds = datetime.now().strftime("_%Y%m%d_%H%M%S") # Suffix _YYYYMMDD_HHMMSS
+    ds = datetime.now().strftime("_%Y%m%d")  # Suffix _YYYYMMDD
     filename = f"{base_filename}{ds}.csv"
-    filepath = "./data/"
-    file = f"{filepath}{filename}"
+    file = f"{data_dir}{filename}"
     gc.save_history_csv(file)
     print(f"{file} has been written!")
 
