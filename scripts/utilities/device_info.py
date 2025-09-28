bbb6927b87a77759f71373cd9799eac933f847b9
David Johns <djjohns42@gmail.com>
2025-09-28 13:07:50 -0500

    update device_info.py

--- scripts/utilities/device_info.py
+++ scripts/utilities/device_info.py
@@ -1,40 +1,54 @@
 import pygmc
+from typing import Any
 
 
-def device_info():
+def device_info() -> dict[str, dict[str, Any]]:
+    """Returns a dictionary of device info."""
     gc = pygmc.GMCSE(port="COM3")
 
     conn_details = gc.get_connection_details()
 
-    device_info = f"""\
-    |------------------------GQ GMC GEIGER COUNTER-----------------------------|
-    |Device Details
-    |--------------------------------------------------------------------------|
-    |Version: {gc.get_version()}
-    |Serial Number: {gc.get_serial()}
-    |---------------------------------------------------------------------------|
-    |COM PORT DETAILS
-    |--------------------------------------------------------------------------|
-    |Port: {conn_details["port"]}
-    |Baudrate: {conn_details["baudrate"]}
-    |Timeout: {conn_details["timeout"]}
-    |Open: {conn_details["is_open"]}
-    |In Waiting: {conn_details["in_waiting"]}
-    |Out Waiting: {conn_details["out_waiting"]}
-    |---------------------------------------------------------------------------|
-    |Sensor Readings
-    |--------------------------------------------------------------------------|
-    |Current Voltage: {gc.get_voltage()}
-    |Current Temp: {gc.get_temp()}
-    |Gyroscope: {gc.get_gyro()}
-    |RTC: {gc.get_datetime()}
-    |CPM: {gc.get_cpm()}
-    |μSv/hr: {gc.get_usv_h()}
-    |------------------------GQ GMC GEIGER COUNTER-----------------------------|
-    """
+    device_info: dict[str, dict[str, Any]] = {
+        "device_details": {
+            "version": gc.get_version(),
+            "serial_number": gc.get_serial(),
+        },
+        "com_port_details": {
+            "port": conn_details["port"],
+            "baudrate": conn_details["baudrate"],
+            "timeout": conn_details["timeout"],
+            "open": conn_details["is_open"],
+            "in_waiting": conn_details["in_waiting"],
+            "out_waiting": conn_details["out_waiting"],
+        },
+        "sensor_readings": {
+            "voltage": gc.get_voltage(),
+            "temp": gc.get_temp(),
+            "gyroscope": gc.get_gyro(),
+            "RTC": gc.get_datetime(),
+            "CPM": gc.get_cpm(),
+            "μSv_hr": gc.get_usv_h(),
+        },
+    }
 
     return device_info
 
 
 if __name__ == "__main__":
-    print(device_info())
+    info = device_info()
+    header = f"|-------------------GQ GMC GEIGER COUNTER----------------------|"
+
+    footer = f"|-------------------GQ GMC GEIGER COUNTER----------------------|"
+
+    sep = f"|--------------------------------------------------------------|"
+
+    print(header)
+
+    for group, group_dict in info.items():
+        print(sep)
+        print(f"|{group}")
+        print(sep)
+        for k, v in group_dict.items():
+            print(f"|{k}: {v}")
+
+    print(footer)
